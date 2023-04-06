3f6548636cd9c7a56799e1446f7f7576cace04a3
clait <stefanomus@fastmail.com>
2023-04-06 10:15:07 +0100

    Initialize project using Create React App

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 3001;
+
+app.post('/api/chat', async (req, res) => {
+    try {
+
+
+        const { message } = req.body;
+
+        const response = await axios.post(
+            'https://api.openai.com/v1/chat/completions',
+            {
+                model: req.body.model,
+                messages: [
+                    {
+                        role: "user",
+                        content: message
+                    }
+                ]
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${req.body.apiKey}`,
+                },
+            }
+        );
+        const reply = response.data.choices[0].message.content;
+
+        if (reply !== '') {
+            res.status(200).json({ reply: reply.trim() });
+        } else {
+            res.status(500).json({ error: 'Something bad happened.' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'An error occurred while processing the request.' });
+    }
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
