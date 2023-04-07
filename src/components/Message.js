9a862fc1eaa61d33ba30de41197669236b7f0ae3
clait <stefanomus@fastmail.com>
2023-04-07 23:51:19 +0100

    Lightbox for images

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,24 +1,44 @@
-import React, { useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 import ReactMarkdown from 'react-markdown';
+import Lightbox from "yet-another-react-lightbox";
+import Download from "yet-another-react-lightbox/plugins/download";
+import "yet-another-react-lightbox/styles.css";
 
 const Message = ({ message, toast }) => {
     const { content, role, timestamp, isImage } = message;
     const isUser = role === 'user';
 
-    const handleDoubleClick = useCallback((e) => {
-        const codeBlock = e.target.closest('pre');
-        if (codeBlock) {
-            const code = codeBlock.textContent;
-            navigator.clipboard.writeText(code).then(
-                () => {
-                    toast.info('Copied code to clipboard');
-                },
-                (err) => {
-                    toast.error('Failed to copy code to clipboard');
-                }
-            );
-        }
-    }, [toast]);
+    const [isLightboxOpen, setIsLightboxOpen] = useState(false);
+
+    function formatDate(timestamp) {
+        return new Date(timestamp).toLocaleTimeString([], {
+            day: '2-digit',
+            month: 'short',
+            year: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            hour12: true,
+        });
+    }
+
+
+    const handleDoubleClick = useCallback(
+        (e) => {
+            const codeBlock = e.target.closest('pre');
+            if (codeBlock) {
+                const code = codeBlock.textContent;
+                navigator.clipboard.writeText(code).then(
+                    () => {
+                        toast.info('Copied code to clipboard');
+                    },
+                    (err) => {
+                        toast.error('Failed to copy code to clipboard');
+                    }
+                );
+            }
+        },
+        [toast]
+    );
 
     return (
         <div
@@ -26,20 +46,37 @@ const Message = ({ message, toast }) => {
             onDoubleClick={handleDoubleClick}
         >
             {isImage ? (
-                <img src={content} alt="Generated content" className="generated-image" />
+                <>
+                    <img
+                        src={content}
+                        alt="Generated content"
+                        className="generated-image"
+                        onClick={() =>{
+                            setIsLightboxOpen(true);
+                            console.log("clicked")}
+                        }
+                    />
+                    {isLightboxOpen && (
+                        <Lightbox
+                            open={isLightboxOpen}
+                            close={() => setIsLightboxOpen(false)}
+                            slides={[
+                                {
+                                src: content,
+                                alt: "Content generated at " + formatDate(timestamp),
+                                downloadFilename: "generated-content-" + formatDate(timestamp),
+                                },
+                            ]}
+                            plugins={[Download]}
+                        />
+                    )}
+                </>
             ) : (
                 <ReactMarkdown>{content}</ReactMarkdown>
             )}
             <span className="message-timestamp">
-                {new Date(timestamp).toLocaleTimeString([], {
-                    day: '2-digit',
-                    month: 'short',
-                    year: '2-digit',
-                    hour: '2-digit',
-                    minute: '2-digit',
-                    hour12: true,
-                })}
-            </span>
+        {formatDate(timestamp)}
+      </span>
         </div>
     );
 };
