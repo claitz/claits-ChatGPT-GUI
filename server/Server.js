1e0a8ef907c2a1d86a135e6ef44158c8d9303cc7
clait <stefanomus@fastmail.com>
2023-04-08 23:32:48 +0100

    Resolved a bug that caused text responses to not be sent correctly

--- server/Server.js
+++ server/Server.js
@@ -86,9 +86,43 @@ io.on('connection', (socket) => {
                 console.log(error)
                 socket.emit('error', {error: error.message});
             }
+        }
+    );
 
-            socket.on('chat message', async (data) => {
-                const {
+    socket.on('chat message', async (data) => {
+        const {
+            apiKey,
+            messageInput,
+            apiUrl,
+            model,
+            temperature,
+            topP,
+            n,
+            stop,
+            maxTokens,
+            presencePenalty,
+            frequencyPenalty,
+            logitBias,
+            user,
+            retryCount,
+            fetchTimeout,
+            readTimeout,
+            retryInterval,
+            totalTime,
+        } = data;
+
+        if (!apiKey) {
+            socket.emit('error', {error: 'No API key provided.'});
+            console.log('No API key provided.')
+            return;
+        }
+
+        let currentBotMessage = '';
+        let isFirstChunk = true;
+
+        try {
+            fetchStreamedChatContent(
+                {
                     apiKey,
                     messageInput,
                     apiUrl,
@@ -107,72 +141,38 @@ io.on('connection', (socket) => {
                     readTimeout,
                     retryInterval,
                     totalTime,
-                } = data;
-
-                if (!apiKey) {
-                    socket.emit('error', {error: 'No API key provided.'});
-                    console.log('No API key provided.')
-                    return;
-                }
-
-                let currentBotMessage = '';
-                let isFirstChunk = true;
-
-                try {
-                    fetchStreamedChatContent(
-                        {
-                            apiKey,
-                            messageInput,
-                            apiUrl,
-                            model,
-                            temperature,
-                            topP,
-                            n,
-                            stop,
-                            maxTokens,
-                            presencePenalty,
-                            frequencyPenalty,
-                            logitBias,
-                            user,
-                            retryCount,
-                            fetchTimeout,
-                            readTimeout,
-                            retryInterval,
-                            totalTime,
-                        },
-                        (content) => {
-                            // onResponse
-                            currentBotMessage += content;
-                            socket.emit('bot message', {
-                                reply: currentBotMessage,
-                                isNewMessage: isFirstChunk,
-                                isFinished: false
-                            });
-                            isFirstChunk = false;
-                        },
-                        () => {
-                            // onFinish
-                            socket.emit('bot message', {
-                                reply: currentBotMessage,
-                                isNewMessage: false,
-                                isFinished: true
-                            });
-                        },
-                        (error) => {
-                            // onError
-                            socket.emit('error', {error: error.message});
-                            console.log(error)
-                        }
-                    );
-                } catch (error) {
+                },
+                (content) => {
+                    // onResponse
+                    currentBotMessage += content;
+                    socket.emit('bot message', {
+                        reply: currentBotMessage,
+                        isNewMessage: isFirstChunk,
+                        isFinished: false
+                    });
+                    isFirstChunk = false;
+                },
+                () => {
+                    // onFinish
+                    socket.emit('bot message', {
+                        reply: currentBotMessage,
+                        isNewMessage: false,
+                        isFinished: true
+                    });
+                },
+                (error) => {
+                    // onError
                     socket.emit('error', {error: error.message});
                     console.log(error)
                 }
-            });
-
-            socket.on('disconnect', () => {
-                console.log('Client disconnected');
-            });
+            );
+        } catch (error) {
+            socket.emit('error', {error: error.message});
+            console.log(error)
         }
-    );
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
 });
